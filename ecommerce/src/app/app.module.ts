4150b6314c33d3f9ab8141c14324d2c93419f2c4
Carlos Torrico Lupe <carlosalbertotorricolupe@gmail.com>
2019-05-08 18:45:02 -0400

    type:feat subject:Agregando layout con footer body:Se agrego el componente layout dentro el cual se creo el componente footer

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component'
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/layout/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
