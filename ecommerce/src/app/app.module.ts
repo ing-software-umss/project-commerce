b1c6d5b2567e6ceb2a282e9f71c80c2ca4537733
Carlos Torrico Lupe <carlosalbertotorricolupe@gmail.com>
2019-05-09 18:29:34 -0400

    type:feat   subject:Agregar iconos   body:Se agregaron los iconos de redes sociales y se modificaron   dependencias en el angular.json

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component'
@@ -14,7 +16,9 @@ import { NavbarComponent } from './components/layout/navbar/navbar.component'
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
