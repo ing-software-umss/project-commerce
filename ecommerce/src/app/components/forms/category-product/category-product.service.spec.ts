42ee73a3038918c2b8386744c4aeeb1dec1034af
Adan Franco <franco.start.moba@gmail.com>
2019-06-11 09:10:33 -0400

    type: fix body: solucionando problemas de categoria producto

--- ecommerce/src/app/components/forms/category-product/category-product.service.spec.ts
+++ ecommerce/src/app/components/forms/category-product/category-product.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CategoryProductService } from './category-product.service';
 
 describe('CategoryProductService', () => {
--- ecommerce/src/app/components/forms/category-product/category-product.service.ts
+++ ecommerce/src/app/components/forms/category-product/category-product.service.ts
@@ -7,6 +7,8 @@ import { map } from 'rxjs/internal/operators/map';
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class CategoryProductService {
   nombreCategoria: string;
   private itemDoc: AngularFirestoreDocument<CategoryProduct>;
@@ -22,23 +24,23 @@ export class CategoryProductService {
     );
   }
 
+
   addCatProduct( catProduct: any ): any {
     return this.db.collection<CategoryProduct>('cat-productos').add(catProduct);
   }
   deleteItem(dato) {
-    console.log('deleteItem', dato.id);
     this.itemDoc = this.db.doc<CategoryProduct>(`cat-productos/${dato.id}`);
     this.itemDoc.delete();
   }
 
+
   updateCatProduc(dato){
     this.itemDoc = this.db.doc<CategoryProduct>(`cat-productos/${dato.id}`);
     this.itemDoc.update(dato);
-    
   // return console.log('deleteItem', this.db.collection<CategoryProduct>('cat-productos').doc('name'));
   }
   cambiar(mensaje: string){
     this.nombreCategoria = mensaje;
   }
-  
 }
+
